8d231cac46734987f3a953434c141001b6b6208e
Eduardo Avelino <avelino.edu@gmail.com>
2022-08-01 10:07:07 -0300

    App Aulas Usando Graphql, tailwindcss, typescript e outros

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "./logo";
+
+export function Header() {
+  return (
+    <header className="w-full py-5 flex itens-center justify-center bg-gray-700 border-b border-gray-600">
+         <Logo/>
+    </header>
+ 
+  )
+}
